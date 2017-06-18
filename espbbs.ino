89d51d44970bb7ad6bf98e8bc3182f141e808b93
Josh M. McKee <josh@snazzware.com>
2017-06-17 17:54:24 -0700

    Refactoring, cleanup, commenting, etc.

--- espbbs.ino
+++ espbbs.ino
@@ -3,6 +3,7 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <stdarg.h>
 
 #include "FS.h"
 
@@ -58,18 +59,18 @@ WiFiClient clients[MAX_CLIENTS];
 BBSClient bbsclients[MAX_CLIENTS];
 
 void setup() {
-    Serial.begin(115200);
-
-    SPIFFS.begin();
-
-    WiFiManager wifiManager;
-    wifiManager.autoConnect();
-    
-    Serial.println("Connection established");
-
-    server.begin();
-    Serial.println("Server started");
-    Serial.setDebugOutput(true);
+  Serial.begin(115200);
+  
+  SPIFFS.begin();
+  
+  WiFiManager wifiManager;
+  wifiManager.autoConnect();
+  
+  Serial.println("Connection established");
+  
+  server.begin();
+  Serial.println("Server started");
+  Serial.setDebugOutput(true);
 }
 
 void sendTextFile(WiFiClient client, String filepath) {
@@ -103,8 +104,17 @@ void sendTextFile(WiFiClient client, String filepath) {
       
     }
     f.close();
-    
   }
+}
+
+void cprintf(int clientNumber, char *fmt, ...) {
+  char buf[255];
+  va_list va;
+  va_start (va, fmt);
+  vsnprintf (buf, sizeof(buf), fmt, va);
+  va_end (va);
+
+  if (clients[clientNumber]) clients[clientNumber].write((uint8_t *)buf, strlen(buf));
 }
 
 void getInput(int clientNumber) {
@@ -114,234 +124,220 @@ void getInput(int clientNumber) {
   bbsclients[clientNumber].inputting = true;
 }
 
+void getInput(int clientNumber, char echo) {
+  getInput(clientNumber);
+  bbsclients[clientNumber].inputEcho = echo;
+}
+
+void action (int clientNumber, int actionId, int stageId) {
+  bbsclients[clientNumber].action = actionId;
+  bbsclients[clientNumber].stage = stageId;
+}
+
+void action (int clientNumber, int actionId) {
+  action(clientNumber, actionId, STAGE_INIT);
+}
+
 void loop() {
   char buf[128]; 
   char charIn;
   char buffer[16];
-  char inBuffer[16];
   char result;
   
   for (int i = 0; i < MAX_CLIENTS; i++) {
       if (clients[i]) {
          if (clients[i].status() == CLOSED) {
             clients[i].stop();
-         }
-
-          switch (bbsclients[i].action) {
-            case BBS_LOGIN:
-              switch (bbsclients[i].stage) {
-                case STAGE_INIT:
-                  sprintf(buf, "Node #%u - Username: ", i);
-                  clients[i].write((uint8_t *)buf, strlen(buf));
-                  getInput(i);
-                  bbsclients[i].stage = BBS_LOGIN_USERNAME;
-                break;
-                case BBS_LOGIN_USERNAME:
-                  if (!bbsclients[i].inputting && strlen(bbsclients[i].input)) {
-                    if (strcmp(bbsclients[i].input, "guest") == 0) {
-                      bbsclients[i].action = BBS_GUEST;
-                      bbsclients[i].stage = STAGE_INIT;
-                    } else 
-                    if (strcmp(bbsclients[i].input, "new") == 0) {
-                      sprintf(buf, "Sorry, new user signups have not been implemented. Try 'guest'.\r\n");
-                      clients[i].write((uint8_t *)buf, strlen(buf));
-                      bbsclients[i].stage = STAGE_INIT;
-                    } else {
-                      sprintf(buf, "Password: ");
-                      clients[i].write((uint8_t *)buf, strlen(buf));
-                      getInput(i);
-                      bbsclients[i].inputEcho = '*';
-                      bbsclients[i].stage = BBS_LOGIN_PASSWORD;
-                    }
-                  }
-                break;
-                case BBS_LOGIN_PASSWORD:
-                  if (!bbsclients[i].inputting) {
-                    if (strlen(bbsclients[i].input)) {
-                      sprintf(buf, "Nope!\r\n");
-                      clients[i].write((uint8_t *)buf, strlen(buf));
-                      bbsclients[i].stage = STAGE_INIT;
-                    } else {
-                      bbsclients[i].stage = STAGE_INIT;
-                    }
-                  }
-                break;
-              }
-            break;
-            case BBS_GUEST:
-              switch (bbsclients[i].stage) {
-                case STAGE_INIT:
-                  sprintf(buf, "Welcome, guest!\r\n");
-                  clients[i].write((uint8_t *)buf, strlen(buf));
-                  bbsclients[i].action = BBS_MAIN;
-                  bbsclients[i].stage = STAGE_INIT;
-                break;
-              }
-            break;
-            case BBS_FILES:
-              switch (bbsclients[i].stage) {
-                case STAGE_INIT: {
-                    sprintf(buf, "Browsing %s\r\n", ((BBSFileClient *)(bbsclients[i].data))->path);
-                    clients[i].write((uint8_t *)buf, strlen(buf));
-                    sprintf(buf, "B) Go Back\r\n");
-                    clients[i].write((uint8_t *)buf, strlen(buf));
-                    Dir dir = SPIFFS.openDir(((BBSFileClient *)(bbsclients[i].data))->path);
-                    int idx = 1;
-                    while (dir.next()) {
-                      sprintf(buf, "%u) %s\r\n", idx++, dir.fileName().c_str());
-                      clients[i].write((uint8_t *)buf, strlen(buf));
-                    }
+         } else {
+            switch (bbsclients[i].action) {
+              case BBS_LOGIN:
+                switch (bbsclients[i].stage) {
+                  case STAGE_INIT:
+                    cprintf(i, "Node #%u - Username: ", i);
                     getInput(i);
-
-                    bbsclients[i].stage = BBS_FILES_LIST;
-                } break;
-                 
-                case BBS_FILES_LIST: {
-                  if (!bbsclients[i].inputting) {
-                    if (strlen(bbsclients[i].input)>0) {
-                      if (toupper(bbsclients[i].input[0]) == 'B') {
-                        bbsclients[i].action = BBS_MAIN;
-                        bbsclients[i].stage = STAGE_INIT;
+                    action(i, BBS_LOGIN, BBS_LOGIN_USERNAME);
+                  break;
+                  case BBS_LOGIN_USERNAME:
+                    if (!bbsclients[i].inputting && strlen(bbsclients[i].input)) {
+                      if (strcmp(bbsclients[i].input, "guest") == 0) {
+                        action(i, BBS_GUEST);
+                      } else 
+                      if (strcmp(bbsclients[i].input, "new") == 0) {
+                        cprintf(i, "Sorry, new user signups have not been implemented. Try 'guest'.\r\n");
+                        action(i, BBS_LOGIN);
                       } else {
-                        int selection = atoi(bbsclients[i].input);
-                        Dir dir = SPIFFS.openDir(((BBSFileClient *)(bbsclients[i].data))->path);
-                        int readIdx = 1;
-                        while (dir.next() && readIdx <= selection) {
-                          if (readIdx == selection) {
-                            sendTextFile(clients[i], dir.fileName());
+                        cprintf(i, "Password: ");
+                        getInput(i, '*');
+                        action(i, BBS_LOGIN, BBS_LOGIN_PASSWORD);
+                      }
+                    }
+                  break;
+                  case BBS_LOGIN_PASSWORD:
+                    if (!bbsclients[i].inputting) {
+                      if (strlen(bbsclients[i].input)) {
+                        cprintf(i, "Nope!\r\n");
+                        action(i, BBS_LOGIN);
+                      } else {
+                        action(i, BBS_LOGIN);
+                      }
+                    }
+                  break;
+                }
+              break;
+              case BBS_GUEST:
+                switch (bbsclients[i].stage) {
+                  case STAGE_INIT:
+                    cprintf(i, "Welcome, guest!\r\n");
+                    action(i, BBS_MAIN, STAGE_INIT);
+                  break;
+                }
+              break;
+              case BBS_FILES:
+                switch (bbsclients[i].stage) {
+                  case STAGE_INIT: {
+                      cprintf(i, "Browsing %s\r\n", ((BBSFileClient *)(bbsclients[i].data))->path);
+                      cprintf(i, "B) Go Back\r\n");
+                      
+                      Dir dir = SPIFFS.openDir(((BBSFileClient *)(bbsclients[i].data))->path);
+                      int idx = 1;
+                      while (dir.next()) {
+                        cprintf(i, "%u) %s\r\n", idx++, dir.fileName().c_str());
+                      }
+                      getInput(i);
+            
+                      action(i, BBS_FILES, BBS_FILES_LIST);
+                  } break;
+                   
+                  case BBS_FILES_LIST: {
+                    if (!bbsclients[i].inputting) {
+                      if (strlen(bbsclients[i].input)>0) {
+                        if (toupper(bbsclients[i].input[0]) == 'B') {   
+                          action(i, BBS_MAIN);
+                        } else {
+                          int selection = atoi(bbsclients[i].input);
+                          Dir dir = SPIFFS.openDir(((BBSFileClient *)(bbsclients[i].data))->path);
+                          int readIdx = 1;
+                          while (dir.next() && readIdx <= selection) {
+                            if (readIdx == selection) {
+                              sendTextFile(clients[i], dir.fileName());
+                            }
+                            readIdx++;
                           }
-                          readIdx++;
+                        }
+                      }
+                      action(i, BBS_FILES);
+                    }
+                  } break;
+                  
+                }
+              break;
+              case BBS_MAIN:
+                switch (bbsclients[i].stage) {
+                  case STAGE_INIT:
+                    cprintf(i, "Main Menu\r\n");
+                    cprintf(i, "1) Log Out\r\n");
+                    cprintf(i, "2) File Library\r\n");
+                    getInput(i);
+                    action(i, BBS_MAIN, BBS_MAIN_SELECT);
+                  break;
+                  case BBS_MAIN_SELECT:
+                    if (!bbsclients[i].inputting) {
+                      if (strlen(bbsclients[i].input)==1) {
+                        switch (bbsclients[i].input[0]) {
+                          case '1':
+                            action(i, BBS_LOGOUT);
+                          break;
+                          case '2':
+                            action(i, BBS_FILES);
+                            if (bbsclients[i].data != NULL) free(bbsclients[i].data);
+                            bbsclients[i].data = malloc(sizeof(struct BBSFileClient));
+                            strcpy(((BBSFileClient *)(bbsclients[i].data))->path, "/files");
+                          break;
                         }
                       }
                     }
-                    bbsclients[i].stage = STAGE_INIT;
-                  }
-                } break;
-                
-              }
-            break;
-            case BBS_MAIN:
-              switch (bbsclients[i].stage) {
-                case STAGE_INIT:
-                      sprintf(buf, "Main Menu\r\n");
-                      clients[i].write((uint8_t *)buf, strlen(buf));
-                      sprintf(buf, "1) Log Out\r\n");
-                      clients[i].write((uint8_t *)buf, strlen(buf));
-                      sprintf(buf, "2) File Library\r\n");
-                      clients[i].write((uint8_t *)buf, strlen(buf));
-                      getInput(i);
-                      bbsclients[i].stage = BBS_MAIN_SELECT;
                   break;
-                  case BBS_MAIN_SELECT:
-                  if (!bbsclients[i].inputting) {
-                    if (strlen(bbsclients[i].input)==1) {
-                      switch (bbsclients[i].input[0]) {
-                        case '1':
-                          bbsclients[i].action = BBS_LOGOUT;
-                        break;
-                        case '2':
-                          bbsclients[i].action = BBS_FILES;
-                          bbsclients[i].stage = STAGE_INIT;
-                          if (bbsclients[i].data != NULL) free(bbsclients[i].data);
-                          bbsclients[i].data = malloc(sizeof(struct BBSFileClient));
-                          strcpy(((BBSFileClient *)(bbsclients[i].data))->path, "/files");
-                          Serial.println(((BBSFileClient *)(bbsclients[i].data))->path);
-                        break;
-                        
-                      }
-                    }
-                    
-                  }
-                break;
-              }
-            break;
-            case BBS_LOGOUT:
-              sprintf(buf, "\r\nNO CARRIER\r\n\r\n");
-              clients[i].write((uint8_t *)buf, strlen(buf));
-              clients[i].stop();
-            break;
-          }
-
-        
-          result = clients[i].read();
-          if (result != 255) {
-            inBuffer[1] = 0;
-            sprintf(buffer, "client %u char %u %c", i, result, result);
-            Serial.println(buffer);
-
-            if (bbsclients[i].inputting) {
-              if (result == 127) {
-                if (bbsclients[i].inputPos>0) {
-                  result = 8; // convert DEL (127) to backspace (8)
-                  bbsclients[i].input[bbsclients[i].inputPos] = 0;
-                  bbsclients[i].inputPos--;
-                } else result = 0;
-              } else 
-              if (result == 13) {
-                if (bbsclients[i].inputPos > 0) {
-                  bbsclients[i].inputting = false;
-                  bbsclients[i].input[bbsclients[i].inputPos] = 0;
-                  clients[i].write("\r\n");
                 }
-                clients[i].flush();
-              } else {
-                if (bbsclients[i].inputPos+1 < MAX_INPUT) {
-                  bbsclients[i].input[bbsclients[i].inputPos++] = result;
-                  if (bbsclients[i].inputEcho) result = bbsclients[i].inputEcho;
-                } else result = 0;
-              }
-  
-              // echo input to client
-              if (result) clients[i].write((char)result);
+              break;
+              case BBS_LOGOUT:
+                cprintf(i, "\r\nNO CARRIER\r\n\r\n");
+                clients[i].stop();
+              break;
             }
-            
-          }
-        
 
+            // See if we have any data pending 
+            result = clients[i].read();
+            if (result != 255) {
+              if (bbsclients[i].inputting) {
+                if (result == 127 || result == 8) { // handle del / backspace
+                  if (bbsclients[i].inputPos>0) {
+                    result = 8; // convert DEL (127) to backspace (8)
+                    bbsclients[i].input[bbsclients[i].inputPos] = 0;
+                    bbsclients[i].inputPos--;
+                  } else result = 0;
+                } else 
+                if (result == 13) {
+                  if (bbsclients[i].inputPos > 0) {
+                    bbsclients[i].inputting = false;
+                    bbsclients[i].input[bbsclients[i].inputPos] = 0;
+                    clients[i].write("\r\n");
+                  }
+                  clients[i].flush();
+                } else {
+                  if (bbsclients[i].inputPos+1 < MAX_INPUT) {
+                    bbsclients[i].input[bbsclients[i].inputPos++] = result;
+                    if (bbsclients[i].inputEcho) result = bbsclients[i].inputEcho;
+                  } else result = 0;
+                }
+            
+                // echo input to client
+                if (result) clients[i].write((char)result);
+              }
+            }
+         }
       }
       else {
-         clients[i] = server.available();
-         if (clients[i]) {
-           clients[i].write(255); // IAC
-            clients[i].write(251); // WILL
-            clients[i].write(1);   // ECHO
-          
-            clients[i].write(255); // IAC
-            clients[i].write(251); // WILL
-            clients[i].write(3);   // suppress go ahead
-          
-            clients[i].write(255); // IAC
-            clients[i].write(252); // WONT
-            clients[i].write(34);  // LINEMODE
+        clients[i] = server.available();
+        if (clients[i]) {
+          // Send telnet configuration - we'll handle echoes, and we do not want to be in linemode.
+          clients[i].write(255); // IAC
+          clients[i].write(251); // WILL
+          clients[i].write(1);   // ECHO
+        
+          clients[i].write(255); // IAC
+          clients[i].write(251); // WILL
+          clients[i].write(3);   // suppress go ahead
+        
+          clients[i].write(255); // IAC
+          clients[i].write(252); // WONT
+          clients[i].write(34);  // LINEMODE
+        
+          // reset bbs client data
+          bbsclients[i].inputting = false;
+          bbsclients[i].inputPos = 0;
+          bbsclients[i].inputEcho = 0;
+          bbsclients[i].input[0] = 0;
+          bbsclients[i].action = BBS_LOGIN;
+          bbsclients[i].stage = STAGE_INIT;
+          bbsclients[i].data = (void *)NULL;
 
-            // reset bbs client data
-            bbsclients[i].inputting = false;
-            bbsclients[i].inputPos = 0;
-            bbsclients[i].inputEcho = 0;
-            bbsclients[i].input[0] = 0;
-            bbsclients[i].action = BBS_LOGIN;
-            bbsclients[i].stage = STAGE_INIT;
-            bbsclients[i].data = (void *)NULL;
-          
-          //
+          // Send the title file
           sendTextFile(clients[i], "/title.ans");
-          
-          sprintf(buf, "Log in as guest, or type new to create an account.\r\n");
-          clients[i].write((uint8_t *)buf, strlen(buf));
 
+          cprintf(i, "Log in as 'guest', or type new to create an account.\r\n");
           clients[i].flush(); // discard initial telnet data from client
-         }
+        }
       }
    }
 
-   // Catch extra callers
-   WiFiClient client = server.available();
-   if (client) {
+  // Catch extra callers
+  WiFiClient client = server.available();
+  if (client) {
     sprintf(buf, "All nodes are currently in use.\r\n");
     client.write((uint8_t *)buf, strlen(buf));
     client.stop();
-   }
+  }
 
-   optimistic_yield(100);
-  
+  // Let ESP8266 do some stuff
+  yield();
 }
